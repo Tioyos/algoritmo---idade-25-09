890dd1a122dc735f2f0df9019368b5e3d97d7088
FELIPE DA SILVA CRUZ BRANDAO <felipe.scbrandao@senacsp.edu.br>
2024-09-25 14:31:57 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//solicitar a idade do usuario
+
+let idade = parseInt(prompt("insira a sua idade"));
+
+//verificar se a idade é maior que 18 anos
+
+if ( idade>= 18) {
+  alert("acesso permitido!");
+}else{
+    alert("acesso não permitido")
+}
+
